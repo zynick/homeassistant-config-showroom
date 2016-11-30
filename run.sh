f3ad75d1b0c05089698ec60da6a35bf569668963
Chan Zi Yuan <ziyuan@zynick.com>
2016-11-30 23:45:27 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-hass --config . --open-ui
+# for localhost development, run this only
+# hass --config . --open-ui
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
+
+source /home/pi/hass/venv/bin/activate
+
+# for debugging purpose only
+hass -c /home/pi/hass/conf &> /var/log/hass.log
+# run this in production
+# hass -c /home/pi/hass/conf
