178054e17903e51e422fc57cd21b83686f775bdf
Chan Zi Yuan <ziyuan@zynick.com>
2016-12-12 23:52:13 +0800

    1. add sleep time 2. clear ftp configuration 3. add FTP_PATH

--- redirect.sh
+++ redirect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-FTP_HOST="ftp.smartboxasia.com"
-FTP_USER="sbacom@smartboxasia.com"
-FTP_PASS="$7E_wE6x3Ra$"
+FTP_HOST=""
+FTP_PATH=""
+FTP_USER=""
+FTP_PASS=""
 
 
 # Export Path
@@ -11,10 +12,13 @@ FTP_PASS="$7E_wE6x3Ra$"
 # Start ngrok in daemon mode
 ngrok http 80 -region ap > /dev/null &
 
+# Waiting for ngrok to start up
+sleep 10
+
 # Generate Redirect HTML
 URL="$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')"
 HTML="<html><head><meta HTTP-EQUIV=\"REFRESH\" content=\"5; url=$URL\"></head><body><h1>hello</h1></body></html>"
 echo $HTML > showroom.html
 
 # Upload Redirect HTML to website
-curl -u '$FTP_USER:$FTP_PASS' -T showroom.html ftp://$FTP_HOST
+curl -u '$FTP_USER:$FTP_PASS' -T showroom.html ftp://$FTP_HOST$FTP_PATH
