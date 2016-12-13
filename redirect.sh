f0bb74529e145683b6dd56ac7dce297664a30926
Chan Zi Yuan <ziyuan@zynick.com>
2016-12-13 19:53:30 +0800

    fix this

--- redirect.sh
+++ redirect.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-FTP_HOST=""
-FTP_PATH=""
+FTP_HOST="ftp.smartboxasia.com"
 FTP_USER=""
 FTP_PASS=""
-
 
 # Export Path
 # export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 
-# Start ngrok in daemon mode
+# Restart ngrok in daemon mode
+pkill ngrok
 ngrok http 80 -region ap > /dev/null &
 
 # Waiting for ngrok to start up
-sleep 10
+sleep 5
 
 # Generate Redirect HTML
 URL="$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')"
-HTML="<html><head><meta HTTP-EQUIV=\"REFRESH\" content=\"5; url=$URL\"></head><body><h1>hello</h1></body></html>"
+HTML="<html><head><meta HTTP-EQUIV=\"REFRESH\" content=\"0; url=$URL\"></head></html>"
 echo $HTML > showroom.html
 
 # Upload Redirect HTML to website
-curl -u '$FTP_USER:$FTP_PASS' -T showroom.html ftp://$FTP_HOST$FTP_PATH
+EXECUTE="curl -u '$FTP_USER:$FTP_PASS' -T showroom.html ftp://$FTP_HOST"
+bash -c "$EXECUTE"
