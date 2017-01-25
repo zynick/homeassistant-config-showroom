e1152cf531a3d4b97e8a7d3a5e48a0bb21f0dcdf
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-25 11:18:39 +0800

    meta tags to not cache the redirect page

--- scripts/redirect.sh
+++ scripts/redirect.sh
@@ -17,7 +17,7 @@ sleep 5
 
 # Generate Redirect HTML
 URL="$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')"
-HTML="<html><head><meta HTTP-EQUIV=\"REFRESH\" content=\"0; url=$URL\"></head></html>"
+HTML="<html><head><meta http-equiv=\"refresh\" content=\"0; url=$URL\"><meta http-equiv=\"pragma\" content=\"no-cache\"><meta http-equiv=\"expires\" content=\"-1\"></head></html>"
 echo $HTML > /var/tmp/showroom.html
 
 # Upload Redirect HTML to website
